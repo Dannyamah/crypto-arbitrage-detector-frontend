ea5a7c670dee6ce15f01ff4ba6da3af2bdb05069
Daniel Amah <dannyamah@gmail.com>
2025-10-07 01:31:53 +0100

    Fa Import Error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
   Navbar,
   ProgressBar,
 } from 'react-bootstrap';
-import { FaSync, FaChartLine, FaInfoCircle } from 'react-icons/fa';
+import { FaSync, FaChartLine, FaInfoCircle, FaSun, FaMoon, FaTelegramPlane, FaTwitter, FaChevronUp, FaChevronDown } from 'react-icons/fa';
 import './App.css';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
